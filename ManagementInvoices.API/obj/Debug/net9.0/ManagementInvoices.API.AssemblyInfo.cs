85a8488ff36091f2a58277b4081aad341d0f828c
Khadija selmani <105151675+khadijaselmani@users.noreply.github.com>
2025-08-03 19:14:13 +0200

    no message

--- ManagementInvoices.API/obj/Debug/net9.0/ManagementInvoices.API.AssemblyInfo.cs
+++ ManagementInvoices.API/obj/Debug/net9.0/ManagementInvoices.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManagementInvoices.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+654f1c1c60f591d5bc9414e234043eab4d12aab4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManagementInvoices.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManagementInvoices.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
